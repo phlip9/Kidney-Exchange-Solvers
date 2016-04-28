b6aae3946c860345173bdfd3a736d15dc3a051ae
Philip Hayes <philiphayes9@gmail.com>
2016-04-28 04:14:25 -0700

    Solve low density problems too

--- solve.py
+++ solve.py
@@ -1,11 +1,10 @@
-from solvers import solve_instance
-
-def read_n(filename):
-    with open(filename) as f:
-        return int(f.readline())
+from solvers import read, solve_instance
+import numpy as np
 
 for i in range(1, 493):
     filename = 'phase1-processed/%d.in' % i
-    n = read_n(filename)
-    if n < 400:
+    A, C = read(filename)
+    n = A.shape[0]
+    d = np.sum(A)/(n*n - n)
+    if n < 400 or d <= 0.05:
         solve_instance(i, 5, 0.05)
